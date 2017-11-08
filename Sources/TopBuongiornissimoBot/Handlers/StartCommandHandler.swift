dbf1e3a5dad7f9b97e7b4df7320a85cf9705cde8
Matteo Piccina <altermatte@gmail.com>
2017-11-08 17:23:20 +0100

    Change start message

--- Sources/TopBuongiornissimoBot/Handlers/StartCommandHandler.swift
+++ Sources/TopBuongiornissimoBot/Handlers/StartCommandHandler.swift
@@ -12,7 +12,7 @@ class StartCommandHandler: Handler {
     static func run(context: Context) -> Bool {
         guard context.privateChat else { return false }
         
-        context.respondAsync("Buongiornissimo ☕️!\nQuesto bot serve a conteggiare i buongiorno scritti nei gruppi, inoltre stila due tipi di classifica:\n● /classifica - Classifica degli utenti per gruppo\n● /gruppi - Classifica globale dei gruppi\n\nPer avviarlo basta aggiungerlo in un gruppo")
+        context.respondAsync("Buongiornissimo ☕️!\n\nQuesto bot serve a conteggiare i buongiorno scritti nei gruppi, inoltre stila due tipi di classifica:\n• /classifica - Classifica degli utenti per gruppo\n• /gruppi - Classifica globale dei gruppi\n\nPer avviarlo basta aggiungerlo in un gruppo")
         
         return true
     }
